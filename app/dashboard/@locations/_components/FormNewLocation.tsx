cb33926b3861576a1f950f623f1060a93d90a74c
GaaelP <gposada24@alumnos.uaq.mx>
2024-10-25 14:28:12 -0600

    implementing cache

--- app/dashboard/@locations/_components/FormNewLocation.tsx
+++ app/dashboard/@locations/_components/FormNewLocation.tsx
@@ -1,11 +1,16 @@
 import { createLocation } from "@/actions/locations/create";
 import { API_URL, TOKEN_NAME } from "@/constants";
-import { Input } from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
 import axios from "axios";
 import { cookies } from "next/headers";
 import SelectManager from "./SelectManager";
 
-export default async function FormNewLocation() {
+export default async function FormNewLocation({
+    searchParams
+}: {
+    searchParams: {[key: string]: string | string[] | undefined}
+}) {
+    if (searchParams.store) return null
     const token = cookies().get(TOKEN_NAME)?.value
     const responseManagers = await axios.get(`${API_URL}/managers`, {
         headers: {
@@ -18,13 +23,14 @@ export default async function FormNewLocation() {
         }
     })
     return (
-        <form action={createLocation}>
+        <form action={createLocation} className="bg-blue-400 py-2 px-4 flex flex-col gap-6 w-full rounded-lg">
+            <h1 className="text-2xl text-white text-center">Crear tienda</h1>
             <Input label="Nombre" name="locationName" />
             <Input label="Dirección" name="locationAddres" />
             <Input label="Latitud" name="locationLat" />
             <Input label="Latitud" name="locationLng" />
             <SelectManager  managers={responseManagers.data} locations={responseLocations.data}/>
-            <button>Subir</button>
+            <Button type="submit" color="primary">Subir</Button>
         </form>
     )
 }
--- app/dashboard/@locations/page.tsx
+++ app/dashboard/@locations/page.tsx
@@ -1,20 +1,19 @@
 import { API_URL, TOKEN_NAME } from "@/constants";
 import { Location } from "@/entities";
 import axios from "axios"
-import { cookies } from "next/headers";
 import SelectLoction from "./_components/SelectLocation";
 import LocationCard from "./_components/LocationCard";
 import FormNewLocation from "./_components/FormNewLocation";
+import DeleteLocationButton from "./_components/DeleteLocationButton";
+import { authHeaders } from "@/helpers/authHeaders";
 const LocationPage = async ({ searchParams }: {
     searchParams: { [key: string]: string | string[] | undefined }
 }) => {
-    const userCookies = cookies()
-    const token = userCookies.get(TOKEN_NAME)?.value
     let { data } = await axios.get<Location[]>(
         `${API_URL}/locations`,
         {
             headers: {
-                Authorization: `Bearer ${token}`
+               ...authHeaders()
             }
         }
     )
@@ -36,7 +35,10 @@ const LocationPage = async ({ searchParams }: {
                 <div className="w-8/12">
                     <LocationCard store={searchParams.store}/>
                 </div>
-                    <FormNewLocation />
+                <div className="w-6/12">
+                    <FormNewLocation searchParams={searchParams}/>
+                </div>
+                <DeleteLocationButton store={searchParams.store}/>
             </div>
         </div>
     )
